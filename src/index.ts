7d43fde7d875b97c5d688bf1dbf86c7b98b1dc14
Peter Colapietro <petercolapietro+github@gmail.com>
2019-06-22 11:06:39 -0400

    feat(tutorial): Adapt part1.html into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,22 @@
 import "phaser";
-// @ts-ignore // this works via webpack file-loader
-import logoImg from "./assets/logo.png";
-
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
   width: 800,
   height: 600,
-  backgroundColor: '#efefef',
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 const game = new Phaser.Game(config);
 
-
-// const imageKey = "logo";
-const cartmanSvgKey = "cartman";
-
-function preload() {
-  // this.load.image(imageKey, logoImg);
-  this.load.svg(cartmanSvgKey, 'src/assets/cartman.svg', {
-    width: 416,
-    height: 388,
-  });
+function preload () {
 }
 
-function create() {
-  // const logo = this.add.image(400, 150, imageKey);
-  const cartman = this.add.image(400, 150, cartmanSvgKey);
+function create () {
+}
 
-  this.tweens.add({
-    // targets: logo,
-    targets: cartman,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+function update () {
 }
